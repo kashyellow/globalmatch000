42435d77eb9ffbcf6b3495fe3d1d1919bca95bb2
kashyellow <2re617@gmail.com>
2026-08-31 15:17:31 -0400

    Create Firebase.js

---
+++ Firebase.js
@@ -0,0 +1,6 @@
+// Firebase template - replace with your config
+console.log("Global Match - Firebase ready placeholder");
+window.GLOBAL_MATCH_CONFIG = {
+  firebase: { apiKey: "REPLACE_ME", authDomain: "REPLACE_ME", projectId: "REPLACE_ME" },
+  adminEmail: "you@example.com"
+};
