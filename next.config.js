75a537e3e447bed88dbc92eaa952f432b66bd14c
kashyellow <2re617@gmail.com>
2026-09-02 13:53:42 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.pravatar.cc', 'images.unsplash.com', 'avatars.githubusercontent.com']
+  }
+}
+module.exports = nextConfig
